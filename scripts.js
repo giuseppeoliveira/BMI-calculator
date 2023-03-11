8445240b94916e7c269bd3e6ac6938dc3d2d3f98
Giuseppe Oliveira <giuseppe_santos@Hotmail.com>
2023-03-11 19:39:36 -0300

    foi utilizada a função parseFloat() para garantir que as entradas de peso e altura sejam convertidas para números decimais. O método toFixed(2) foi utilizado para arredondar o resultado do cálculo do IMC para 2 casas decimais. Além disso, as condições do if e do else if foram simplificadas, de forma a tornar o código mais legível e de fácil manutenção. Por fim, utilizamos o método on() para a associação de eventos, que é uma maneira mais atualizada e recomendada em relação ao método bind().

--- scripts.js
+++ scripts.js
@@ -1,40 +1,31 @@
-// IMC = Peso / (Altura²)
-
-$(function(){
-    $('button').bind('click', function(){
-        var altura = $('#altura').val();
-        var peso = $('#peso').val();
-
-        altura = altura.replace(',','.');
-        peso = peso.replace(',','.');
-
-        var imcA = peso/(altura*altura);
-        var imc = +parseFloat((imcA).toFixed(2));
-
-
-        if(imc <16) {
-            var traducao = 'Desnutrido';
-        } else if(imc >=16 && imc <16.99){
-            var traducao = 'Semi-desnutrição';
-        } else if(imc >=17 && imc <18.49){
-            var traducao = 'Baixo peso';
-        } else if(imc >=18.50 && imc <24.99){
-            var traducao = 'Peso normal';
-        } else if(imc >=25 && imc <29.99){
-            var traducao = 'Sobrepeso';
-        } else if(imc >=30 && imc <34.99){ 
-            var traducao = 'Obesidade grau I';
-        } else if(imc >=35 && imc <39.99){
-            var traducao = 'Obesidade grau II';
-        } else if(imc >=40){
-            var traducao = 'Obesidade grau III';
-        }
-
-        $('#resultado').css("display", "block").html("Seu IMC é "+imc+" kg/m² e seu status é: "+traducao);
-        $('.button, input, h1, h2').css("display", "none");
-        $('.button2').css("display", "block");
-        
-
+$(function() {
+    $('button').on('click', function() {
+      var altura = parseFloat($('#altura').val().replace(',', '.')).toFixed(2);
+      var peso = parseFloat($('#peso').val().replace(',', '.')).toFixed(2);
+      var imc = (peso / (altura * altura)).toFixed(2);
+      var traducao;
+  
+      if (imc < 16) {
+        traducao = 'Desnutrido';
+      } else if (imc < 17) {
+        traducao = 'Semi-desnutrição';
+      } else if (imc < 18.5) {
+        traducao = 'Baixo peso';
+      } else if (imc < 25) {
+        traducao = 'Peso normal';
+      } else if (imc < 30) {
+        traducao = 'Sobrepeso';
+      } else if (imc < 35) {
+        traducao = 'Obesidade grau I';
+      } else if (imc < 40) {
+        traducao = 'Obesidade grau II';
+      } else {
+        traducao = 'Obesidade grau III';
+      }
+  
+      $('#resultado').css('display', 'block').html('Seu IMC é ' + imc + ' kg/m² e seu status é: ' + traducao);
+      $('.button, input, h1, h2').css('display', 'none');
+      $('.button2').css('display', 'block');
     });
-});
-
+  });
+  
